cfead4b6f50b555d8a1502a9f49bb4b98e531070
Bhavya Popat <bhavyabpopat7777@gmail.com>
2023-06-01 12:20:07 +0530

    removed unnecessary comments

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-import { create } from "domain";
 import express from "express";
-import { cp } from "fs";
 import mongoose from "mongoose";
 const createDynamicModel = require("./modules/dynamicModel");
 
@@ -18,46 +16,9 @@ attemptConnection()
   .then(() => console.log("MongoDB Connected!"))
   .catch((err) => console.log("MongoDB Connection Error: " + err));
 
-// // Define the item schema
-// const itemSchema = new mongoose.Schema({
-//   name: String,
-//   description: String,
-// });
-
-// // Create the item model
-// const Item = mongoose.model("Item", itemSchema);
-// Create a generic model without a predefined schema
-
-const Item = mongoose.model(
-  "Item",
-  new mongoose.Schema({}, { strict: false }),
-  "items"
-);
-
-// Retrieve all items
-// app.get("/api/items", async (req, res) => {
-//   try {
-//     const items = await Item.find();
-//     res.json(items);
-//   } catch (error) {
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// });
-
 app.get("/", async (req, res) => {
   res.json("API is Live!");
 });
-
-// Save an item
-// app.post("/api/items", async (req, res) => {
-//   try {
-//     const newItem = new Item(req.body);
-//     await newItem.save();
-//     res.json(newItem);
-//   } catch (error) {
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// });
 
 app.post("/api/:collectionName", async (req, res) => {
   try {
@@ -98,7 +59,6 @@ app.put("/api/:collectionName/:itemId", async (req, res) => {
     const collectionName = req.params.collectionName;
     const itemId = req.params.itemId;
     const updatedData = req.body;
-
     const Model = createDynamicModel(collectionName);
     const item = await Model.findByIdAndUpdate(itemId, updatedData, {
       new: true,
@@ -114,8 +74,6 @@ app.delete("/api/:collectionName/:itemId", async (req, res) => {
   try {
     const collectionName = req.params.collectionName;
     const itemId = req.params.itemId;
-    const updatedData = req.body;
-
     const Model = createDynamicModel(collectionName);
     const item = await Model.findByIdAndDelete(itemId);
     res.json(item);
